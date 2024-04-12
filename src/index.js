179e900143ebfd15738d8053d0fb48eacfd04ce0
Ritikbadhan <ritik8054@email.com>
2024-04-12 23:55:11 +0530

    utls code setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from 'dotenv';
 import connectDb from "./db/index.js";
+import { app } from './app.js';
 dotenv.config({
     path:'./env'
 })
 connectDb()
-
+.then(()=>{
+    app.listen(process.env.PORT || 5000 ,()=>{
+        console.log(`Server Started ${process.env.PORT || 5000}`);
+    })
+})
+.catch((e)=>{
+    console.log("DB connection failed !!!" ,e);
+})
 
 
 
