94cb0dbe18dfcf28a601143aa49db424c71fb7d0
Ritikbadhan <ritik8054@email.com>
2024-04-14 18:41:14 +0530

    setup done and first api tested

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-export const app = express()
+const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -12,3 +12,13 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
+
+
+// Routes import
+
+import userRouter from './routes/user.routs.js'
+
+// Route Declartion
+app.use("/api/v1/users", userRouter)
+
+export {app}
